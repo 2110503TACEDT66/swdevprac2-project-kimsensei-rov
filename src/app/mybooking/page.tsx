c29e67af32cd7d5dfef11e90f85f84d35b6254af
0PO9 <120458903+0PO9@users.noreply.github.com>
2024-03-26 05:11:45 +0700

    KUYYYY

--- src/app/mybooking/page.tsx
+++ src/app/mybooking/page.tsx
@@ -1,11 +1,8 @@
 "use client"
-import BookingList from "@/components/BookingList"
-
 
 export default function BookingPage () {
     return (
         <main>
-            <BookingList></BookingList>
         </main>
     )
 }
