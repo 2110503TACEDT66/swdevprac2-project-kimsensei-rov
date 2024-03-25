3b33d0f75709e1b6b84e0ca49850ccd7d1b3f9cb
PiyawatArt <piyawat.manu@gmail.com>
2024-03-25 22:05:01 +0700

    fix readme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import TopMenu from '@/components/TopMenu'
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import NextAuthProvider from "@/providers/NextAuthProvider";
+import ReduxProvider from '@/redux/ReduxProvider';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -23,11 +25,13 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NextAuthProvider session={session}>
-          <TopMenu />
-          {children}
+        <ReduxProvider>
+      <NextAuthProvider session={session}>
+        <TopMenu/>
+        {children}
         </NextAuthProvider>
-      </body>
+        </ReduxProvider>
+        </body>
     </html>
   )
 }
---
+++ src/db/models/Booking.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const BookingSchema = new mongoose.Schema({
+    bookingฺBegin: {
+        type: Date,
+        required: [true, 'Please add a booking begin date']
+    },
+    bookingEnd: {
+        type: Date,
+        required: [true, 'Please add a booking end date']
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    hotel: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Hotel',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Booking = mongoose.models.Booking || mongoose.model('Booking', BookingSchema);
+export default Booking;
