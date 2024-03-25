0217fe5d893608e83da5be8969b2da5c7ef30daf
khongkrapuns <khongkrapuns@gmail.com>
2024-03-25 22:30:21 +0700

    buttonadjust

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -32,7 +32,7 @@ export default function Banner(){
                 : null
             }
             <button className='bg-white text-cyan-600 border border-cyan-600
-                font-semibold py-2 px-2 m-2 rounded z-30 absolute bottom-0 right-0
+                font-semibold py-2 px-2 m-2 rounded z-30 absolute bottom-0 right-10
                 hover:bg-cyan-600 hover:text-white hover:border-transparent'
                 onClick={(e)=>{e.stopPropagation();router.push('/hospital')}}
                 >
