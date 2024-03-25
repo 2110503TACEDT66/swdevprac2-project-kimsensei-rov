41fac64003243267f08a8220358235907c14e055
khongkrapuns <khongkrapuns@gmail.com>
2024-03-26 01:12:36 +0700

    trypadding

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -14,9 +14,9 @@ export default async function profile () {
         <main className="bg-state-100 m-5 p-5">
             <div className="text-2xl">{profile.data.name}</div>
             <table className="table-auto border-spacing-2"><tbody>
-                <tr><td>Email</td><td>{profile.data.email}</td></tr>
-                <tr><td>Tel.</td><td>{profile.data.telephone}</td></tr>
-                <tr><td>Member Since</td><td>{createdAt.toString()}</td></tr>
+                <tr><td>Email</td><td className="pl-4">{profile.data.email}</td></tr>
+                <tr><td>Tel.</td><td className="pl-4">{profile.data.telephone}</td></tr>
+                <tr><td>Member Since</td><td className="pl-4">{createdAt.toString()}</td></tr>
                 </tbody></table>
         </main>
     );
