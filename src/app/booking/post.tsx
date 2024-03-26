417bfe04d0e4158741529cae607aeec5b5599181
khongkrapuns <khongkrapuns@gmail.com>
2024-03-26 13:34:40 +0700

    canadd

--- src/app/booking/post.tsx
+++ src/app/booking/post.tsx
@@ -7,8 +7,8 @@ import { BookingItem } from "../../../interface"
 
 export default async function getHos({item}: {item: BookingItem}) {
     const session = await getServerSession(authOptions)
-    if(!session || !session.user.token) return null
-    const profile = await getUserProfile(session.user.token)
-    postBooking(item, profile.token)
-    alert("booking add");
+    if(!session || !session.user.token) return null;
+    //const profile = await getUserProfile(session.user.token)
+    postBooking(item, session.user.token)
+    //alert("booking add");
 }
--- src/libs/postBooking.tsx
+++ src/libs/postBooking.tsx
@@ -1,7 +1,8 @@
 import { BookingItem } from "../../interface"
 
 export default async function postBooking(bookingData: BookingItem, token: string) {
-    const response = await fetch(`https://backend-hotel-booking.vercel.app/api/v1/hotel/${bookingData.hotel}/bookings/`, {
+    console.log(JSON.stringify(bookingData));
+    const response = await fetch(`https://backend-hotel-booking.vercel.app/api/v1/hotels/${bookingData.hotel}/bookings/`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -10,6 +11,7 @@ export default async function postBooking(bookingData: BookingItem, token: strin
         body: JSON.stringify(bookingData),
     })
     if (!response.ok) {
+        console.log(response);
         throw new Error("Failed to post booking")
     }
 
